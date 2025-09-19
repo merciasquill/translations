aae31d6f5ea88a64f89f6f0c34d88a3dc9bc776d
arib-alam <aribqis@gmail.com>
2025-09-19 17:06:09 -0500

    feat: added french locales

--- sort.mjs
+++ sort.mjs
@@ -1,16 +1,21 @@
-import { readdirSync, writeFileSync } from "fs";
+import { readdirSync, readFileSync, writeFileSync } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 const sourceFiles = readdirSync("./sources");
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 for (const source of sourceFiles) {
-  const { default: importedData } = await import(`./sources/${source}`, {
-    assert: { type: "json" },
-  });
+  const sourcePath = join(__dirname, "sources", source);
+    const importedData = JSON.parse(readFileSync(sourcePath, "utf8"));
+
 
   const sortedData = {};
 
   const keys = Object.keys(importedData).sort();
   for (const key of keys) sortedData[key] = importedData[key];
-
+ 
   writeFileSync(`./sources/${source}`, JSON.stringify(sortedData, null, 2));
 }
--- sync.mjs
+++ sync.mjs
@@ -1,46 +1,46 @@
-import { readdirSync, writeFileSync } from "fs";
+import { readdirSync, readFileSync, writeFileSync } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 const sourceFiles = readdirSync("./sources");
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 for (const source of sourceFiles) {
-    const { default: importedData } = await import(`./sources/${source}`, {
-        assert: { type: "json" },
-    });
+  const sourcePath = join(__dirname, "sources", source);
+  const importedData = JSON.parse(readFileSync(sourcePath, "utf8"));
 
-    const languageData = {};
+  const languageData = {};
 
-    for (const key in importedData) {
-        const formattedKey = formatKey(source, key);
-        languageData[formattedKey] = { "en-GB": importedData[key] };
+  for (const key in importedData) {
+    const formattedKey = formatKey(source, key);
+    languageData[formattedKey] = { "en-GB": importedData[key] };
+  }
+
+  const localeFolders = readdirSync("./locales");
+
+  console.log(localeFolders)
+
+  for (const locale of localeFolders) {
+    const localePath = join(__dirname, "locales", locale, source);
+    const importedLocaleData = JSON.parse(readFileSync(localePath, "utf8"));
+
+    for (const key in importedLocaleData) {
+      const formattedKey = formatKey(source, key);
+
+      if (!languageData[formattedKey]) continue;
+
+      if (importedLocaleData[key] !== languageData[formattedKey]["en-GB"]) {
+        languageData[formattedKey][locale] = importedLocaleData[key];
+      }
     }
+  }
 
-    const localeFolders = readdirSync("./locales");
-
-    for (const locale of localeFolders) {
-        const { default: importedLocaleData } = await import(
-            `./locales/${locale}/${source}`,
-            {
-                assert: { type: "json" },
-            }
-        );
-
-        for (const key in importedLocaleData) {
-            const formattedKey = formatKey(source, key);
-
-            if (!languageData[formattedKey]) continue;
-
-            if (
-                importedLocaleData[key] !== languageData[formattedKey]["en-GB"]
-            ) {
-                languageData[formattedKey][locale] = importedLocaleData[key];
-            }
-        }
-    }
-
-    writeFileSync(`./json/${source}`, JSON.stringify(languageData, null, 2));
+  writeFileSync(`./json/${source}`, JSON.stringify(languageData, null, 2));
 }
 
 function formatKey(base, key) {
-    const baseWithoutExtension = base.replace(".json", "");
-    return key.replace(baseWithoutExtension + ".", "");
+  const baseWithoutExtension = base.replace(".json", "");
+  return key.replace(baseWithoutExtension + ".", "");
 }
